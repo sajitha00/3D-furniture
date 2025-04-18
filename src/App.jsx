31af595c379931f9de478cd003dcba73e81a0c9f
KalanaBamunuarachchi <kalana.bamunuarachchi@outlook.com>
2025-04-18 11:57:52 +0530

    project str

--- src/App.jsx
+++ src/App.jsx
@@ -25,9 +25,7 @@ function App() {
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      </div>
+      
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
