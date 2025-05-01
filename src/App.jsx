5faa8188e697d6a7b6e2fc5ee868a8fdca489912
sajitha00 <sajithageevinda.sd@gmail.com>
2025-05-02 03:10:07 +0530

    added supabase db and backend for sign up and sign in pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import Home from './pages/Home'
-import Signup from './pages/Signup'
-import Signin from './pages/Signin'
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Signin from "./pages/Signin";
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
+      {/* <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/signup" element={<Signup />} />
+        <Route path="/Signup" element={<Signup />} />
         <Route path="/Signin" element={<Signin />} />
-      </Routes>
+      </Routes> */}
+      <Signup />
+      <Signin />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
